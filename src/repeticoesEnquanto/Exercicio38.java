87c048a2f6d3255763181b1ff234031a93a0ebfb
Rodrigo D. Oliveira <rodrigo.silvadeoliveira324@gmail.com>
2022-06-26 22:26:08 -0300

    iniciando repeticoes enquanto

---
+++ src/repeticoesEnquanto/Exercicio38.java
@@ -0,0 +1,30 @@
+package repeticoesEnquanto;
+
+import java.util.Scanner;
+
+//  Escreva um programa que mostre na tela a seguinte contagem
+
+public class Exercicio38 {
+	
+	public int exercicio38(int contagem_exerc38) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Exercício 38 -  Escreva um programa que mostre na tela a seguinte contagem\n");
+		
+		System.out.println("Informe um digíto inteiro --> ");
+		contagem_exerc38 = input.nextInt();
+		
+		while (contagem_exerc38 < 12) {
+			
+			System.out.printf("%d ", contagem_exerc38);
+			contagem_exerc38 = contagem_exerc38 + 1;
+			
+		}
+		
+		System.out.println("Acabou!");
+		
+		return 0;
+	}
+
+}
---
+++ src/repeticoesEnquanto/Main.java
@@ -0,0 +1,16 @@
+package repeticoesEnquanto;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		int contagem_exerc38 = 0;
+		Exercicio38 exercicio38 = new Exercicio38();
+		int resultadoExerc38 = exercicio38.exercicio38(contagem_exerc38);
+
+	}
+}
